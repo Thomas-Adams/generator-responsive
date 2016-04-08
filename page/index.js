7a29237325f115526cee1432e5dd132c68139ad1
heinrich.adams@gmail.com <heinrich.adams@gmail.com>
2016-04-08 09:24:47 +0800

    added first generator for page

--- page/index.js
+++ page/index.js
@@ -1,19 +1,33 @@
 'use strict';
 var util = require('util'),
     path = require('path'),
+    fs = require('fs'),
     yeoman = require('yeoman-generator'),
     htmlWiring = require('html-wiring'),
     mkdirp = require('mkdirp'),
-    nunjucks = require('nunjucks');
+    nunjucks = require('nunjucks'),
+    chalk = require('chalk');
+
+var globalTransitionEffects = ['moveFromRight' ,'moveFromLeft' ,'moveFromBottom' ,'moveFromTop' ,'moveFromRight ontop' ,'moveFromLeft ontop' ,'moveFromBottom ontop' ,'moveFromTop ontop' ,'moveFromRightFade' ,'moveFromLeftFade' ,'moveFromBottomFade' ,'moveFromTopFade' ,'moveFromRight' ,'moveFromLeft' ,'moveFromBottom' ,'moveFromTop' ,'moveFromRight ontop' ,'moveFromLeft ontop' ,'moveFromBottom ontop' ,'moveFromTop ontop' ,'scaleUpDown delay300' ,'scaleUp delay300' ,'scaleUp Left' ,'scaleUp Right' ,'scaleUp Top' ,'scaleUp Bottom' ,'scaleUpCenter delay400' ,'moveFromRight delay200 ontop' ,'moveFromLeft delay200 ontop' ,'moveFromTop delay200 ontop' ,'moveFromBottom delay200 ontop' ,'flipInLeft delay500' ,'flipInRight delay500' ,'flipInBottom delay500' ,'flipInTop delay500' ,'scaleUp' ,'rotateInNewspaper delay500' ,'moveFromRight' ,'moveFromLeft' ,'moveFromBottom' ,'moveFromTop' ,'rotatePullRight delay180' ,'rotatePullLeft delay180' ,'rotatePullBottom delay180' ,'rotatePullTop delay180' ,'moveFromRightFade' ,'moveFromLeftFade' ,'moveFromBottomFade' ,'moveFromTopFade' ,'rotateUnfoldLeft' ,'rotateUnfoldRight' ,'rotateUnfoldTop' ,'rotateUnfoldBottom' ,'rotateRoomLeftIn' ,'rotateRoomRightIn' ,'rotateRoomTopIn' ,'rotateRoomBottomIn' ,'rotateCubeLeftIn' ,'rotateCubeRightIn' ,'rotateCubeTopIn' ,'rotateCubeBottomIn' ,'rotateCarouselLeftIn' ,'rotateCarouselRightIn' ,'rotateCarouselTopIn' ,'rotateCarouselBottomIn' ,'rotateSidesIn delay200' ,'rotateSlideIn'];
 
 var PageGenerator = module.exports = function PageGenerator(args, options, config) {
 	  // By calling `NamedBase` here, we get the argument to the subgenerator call
-	  // as `this.name`.
+	  // as `this.name`.	 
 	  yeoman.Base.apply(this, arguments);
 };
 
 util.inherits(PageGenerator, yeoman.Base);
 
+
+
+PageGenerator.prototype.initializing = function() {
+		this.menus = [];	
+		this.options.leftTransition=null;
+		this.options.rightTransition=null;
+		this.options.topTransition=null;
+		this.options.menuTransition=null;
+		this.options.charset="utf-8";
+};
 
 PageGenerator.prototype.prompting = {
 		
@@ -23,12 +37,48 @@ PageGenerator.prototype.prompting = {
             var prompt = [{
                 type: 'input',
                 name: 'pageName',
-                message: 'Enter the complete path to the page',
-                'default': path.basename(process.cwd())
+                message: chalk.green.bold('Enter the complete path to the page,\nstarting from the app root\nlike for example ./cn/index.html: '),   
+                validate : function(input) {
+                	try {
+                		if(input!=null && input!='' && input!='undefined') {
+                			
+                			if(input.startsWith("./")) {
+                				path.normalize(input);
+                			} else if (input.startsWith("/")) {
+                				path.normalize("." + input);
+                			} else if (input.startsWith(".")) {
+                				path.normalize("./" + input.subsstring(1));
+                			} else {
+                				path.normalize("./" + input);
+                			}                			
+                			return true;
+                		} else {
+                			console.log(chalk.red.bold('Page name cannot be empty!'));
+                			return chalk.red.bold('Page name cannot be empty!');
+                		}
+                		
+                	} catch (err) {
+                		console.log(chalk.red.bold('Error in path expression : ' +err));
+                		return chalk.red.bold('Error in path expression : ' +err);
+                	}
+                	
+                	
+                	
+                }
             }];
 
             this.prompt(prompt, function(response) {
-                this.options.pageName = response.pageName;
+            	var input = response.pageName;
+            	if(input.startsWith("./")) {
+            		this.options.pageName=path.normalize(input);
+    			} else if (input.startsWith("/")) {
+    				this.options.pageName=path.normalize("." + input);
+    			} else if (input.startsWith(".")) {
+    				this.options.pageName=path.normalize("./" + input.subsstring(1));
+    			} else {
+    				this.options.pageName=path.normalize("./" + input);
+    			}    
+            	console.log(this.options.pageName);
                 done();
             }.bind(this));
         },
@@ -40,9 +90,9 @@ PageGenerator.prototype.prompting = {
              var prompt = [{
 	            type: 'list',
 	            name: 'pageTransition',
-	            message: 'Select the page transition you want',
+	            message: chalk.green.bold('Select the page transition you want: '),
 	            'default': 'None',
-	            choices: ['None', 'vertical', 'horizontal']
+	            choices: ['None', 'vertical', 'horizontal','infinite scrolling']
         	}];
 
             this.prompt(prompt, function(response) {
@@ -50,6 +100,53 @@ PageGenerator.prototype.prompting = {
                 done();
             }.bind(this));
 		},
+		
+		fonts : function() {
+			var done = this.async();
+			
+			var prompt = [{
+	        	type: 'checkbox',
+	            name: 'fonts',
+	            message: chalk.green.bold('Select the fonts you want (note : font awesome is mandatory): '),
+	            'default': ['font-awesome'],
+	            choices: ['bmw-fonts', 'font-awesome']
+	        }];
+			 
+			this.prompt(prompt, function(response) {
+				this.options.fonts = response.fonts;
+	            done();
+	        }.bind(this));
+		},
+		
+		pageTitle : function() {
+            var done = this.async();
+
+            var prompt = [{
+                type: 'input',
+                name: 'pageTitle',
+                message: chalk.green.bold('Enter the title of the page: ')
+            }];
+
+            this.prompt(prompt, function(response) {
+                this.options.pageTitle = response.pageTitle;
+                done();
+            }.bind(this));
+        },
+        
+        pageDescription : function() {
+            var done = this.async();
+
+            var prompt = [{
+                type: 'input',
+                name: 'pageDescription',
+                message: chalk.green.bold('Enter the description of the page: ')
+            }];
+
+            this.prompt(prompt, function(response) {
+                this.options.pageDescription = response.pageDescription;
+                done();
+            }.bind(this));
+        },
         
         menuPosition :  function()  { 
         	var done = this.async();
@@ -57,15 +154,70 @@ PageGenerator.prototype.prompting = {
 	        var prompt = [{
 	        	type: 'list',
 	            name: 'menuPosition',
-	            message: 'Select the menu position you want',
+	            message: chalk.green.bold('Select the menu position you want: '),
 	            'default': 'Left',
 	            choices: ['Left', 'Top', 'Right']
-	        }];
+	        }, {
+	        		when: function(response) {
+	        				if (response.menuPosition === 'Left') {
+	        					return true;
+	        				} else {
+	        					return false;
+	        				}
+	        		},
+	        		
+	        		type: 'list',
+	        		name: 'leftTransition',
+	        		message: chalk.green.bold('Select the menu transition you want: '),
+	        		'default': 'Show',
+	 	            choices: ['Push', 'Show']
+	        }, {
+		        when: function(response) {
+	    				if (response.menuPosition === 'Right') {
+	    					return true;
+	    				} else {
+	    					return false;
+	    				}
+	    			},
+	    		
+	    		type: 'list',
+	    		name: 'rightTransition',
+	    		message: chalk.green.bold('Select the menu transition you want: '),
+	    		'default': 'Show',
+		        choices: ['Push', 'Show']
+	        }, {
+        		when: function(response) {
+    				if (response.menuPosition === 'Top') {
+    					return true;
+    				} else {
+    					return false;
+    				}
+	    		},	    		
+	    		type: 'list',
+	    		name: 'topTransition',
+	    		message: chalk.green.bold('Select the menu transition you want: '),
+	    		'default': 'Show',
+		        choices: ['Show']		        
+        	}];
+	        
 	        this.prompt(prompt, function(response) {
-            	this.options.menuPosition = response.menuPosition;
+	        	this.options.menuPosition = response.menuPosition;
+	            this.options.leftTransition = response.leftTransition;
+	            this.options.rightTransition = response.rightTransition;
+	            this.options.topTransition = response.topTransition;
+	            if(response.leftTransition) {
+	            	this.options.menuTransition=response.leftTransition;
+	            }
+	            if(response.topTransition) {
+	            	this.options.menuTransition=response.topTransition;
+	            }
+	            if(response.rightTransition) {
+	            	this.options.menuTransition=response.rightTransition;
+	            }	            
                 done();
             }.bind(this));
         },
+
         
         accordionMenu :  function()  { 
         	var done = this.async();
@@ -73,8 +225,8 @@ PageGenerator.prototype.prompting = {
 	        var prompt = [{
 	        	type: 'list',
 	            name: 'accordionMenu',
-	            message: 'Select the type of the menu you want',
-	            'default': 'Left',
+	            message: chalk.green.bold('Select the type of the menu you want: '),
+	            'default': 'Flat',
 	            choices: ['Flat', 'Accordion']
 	        }];
 	        this.prompt(prompt, function(response) {
@@ -84,122 +236,168 @@ PageGenerator.prototype.prompting = {
 	        
         },
         
-        slideCount : function() {
-                var done = this.async();
-
-                var prompt = [{
-                    type: 'input',
-                    name: 'slideCount',
-                    message: 'Enter number of sections',
-                    'default': '2'
-                }];
-
-                this.prompt(prompt, function(response) {
-                    this.options.slideCount = response.slideCount;
-                    done();
-                }.bind(this));
+        
+   
+        globalTransitionEffect : function() {
+        	var done = this.async();
+        	
+        	var prompt = [{
+	        	type: 'list',
+	            name: 'globalTransitionEffect',
+	            message: chalk.green.bold('Select the global transition effect you want: '),
+	            'default': 'moveFromRight',
+	            choices: globalTransitionEffects
+	        }];
+        	
+        	this.prompt(prompt, function(response) {
+                this.options.globalTransitionEffect = response.globalTransitionEffect;
+                this.options.globalTransitionEffectIndex= globalTransitionEffects.indexOf(this.options.globalTransitionEffect);
+                done();
+            }.bind(this));
         },
         
-        globalTransitionEffect : {
+        
+        usedComponents : function() {
+        	var done = this.async();
         	
+        	var prompts = [{
+                type: 'checkbox',
+                name: 'usedComponents',
+                message: chalk.green.bold('Which components do you want to use: '),
+                'default' : [],
+                choices : ['Cards', 'Carousel', 'Paragraphs']
+            }];
+            
+            this.prompt(prompts, function(response) {
+                this.options.usedComponents = response.usedComponents;
+                done();
+            }.bind(this));
+            
+        },
+        
+        gsapLibraries : function() {
+        	var done = this.async();
         	
+        	var prompts = [{
+                type: 'checkbox',
+                name: 'gsapLibraries',
+                message: chalk.green.bold('Which GSAP libraries do you want to use: '),
+                'default' : [],
+                choices : ['jquery.gsap.js','TimelineLite.js', 'TweenLite.js','TimelineMax.js', 'TweenMax.js']
+            }];
+            
+            this.prompt(prompts, function(response) {
+                this.options.gsapLibraries = response.gsapLibraries;
+                done();
+            }.bind(this));            
+        },
+        
+        menuTitle :  function()  { 
+        	var done = this.async();
+
+	        var prompt = [{
+	        	type: 'input',
+	            name: 'menuTitle',
+	            message: chalk.green.bold('Enter the title of the menu you want: ')	            
+	        }];
+	        this.prompt(prompt, function(response) {
+            	this.options.menuTitle = response.menuTitle;
+                done();
+            }.bind(this));
+	        
+        },
+        
+        menuEntries : function() {   
+        	var done =  this.async(), self =this;
         	
+            var prompts = [{
+                type: 'input',
+                name: 'menuEntryTitle',
+                message: chalk.green.bold('Enter the menu title: '),
+            }, {
+            	type: 'input',
+                name: 'menuEntryBackgroundImage',
+                message: chalk.green.bold('Enter the corresponding background image url: '),
+            }, {
+              type: 'confirm',
+              name: 'continueMenuEntries',
+              message: chalk.green.bold('add another menu entry? Y/N :'), 
+              'default' : false	  
+            }];
+            
+            
+            function responseHandler(props) {
+            	//console.log("answer: " + props.continueMenuEntries);
+            	//console.log("menus: " + self.menus.join(', '));
+            	var menuEntry ={};
+            	
+            	if(props.menuEntryTitle) {
+            		menuEntry.title = props.menuEntryTitle;
+            		menuEntry.backGroundImage = props.menuEntryBackgroundImage;
+            		self.menus.push(menuEntry);
+            		//console.log("menus: " + self.menus.join(', '));
+            	}
+            	if(props.continueMenuEntries) {
+            		self.prompt(prompts, responseHandler);
+            	} else {
+            		self.options.menus = self.menus;
+            		done();
+            	}
+            }
+             
+            this.prompt(prompts, function (props) {            	
+            	responseHandler(props);
+            }.bind(this));
+            
+        },
+        
+        headerText : function(){
+        	var done = this.async();
+
+	        var prompt = [{
+	        	type: 'input',
+	            name: 'headerText',
+	            message: chalk.green.bold('Enter the header text you want: ')	            
+	        }];
+	        this.prompt(prompt, function(response) {
+            	this.options.headerText = response.headerText;
+                done();
+            }.bind(this));
+        },
+        
+        logoPath : function(){
+        	var done = this.async();
+
+	        var prompt = [{
+	        	type: 'input',
+	            name: 'logoPath',
+	            message: chalk.green.bold('Enter the path to the logo image starting from root: ')	            
+	        }];
+	        this.prompt(prompt, function(response) {
+            	this.options.logoPath = response.logoPath;
+                done();
+            }.bind(this));
         }
         
         
-		
-			
 };
 
 
 
-
-/*
-module.exports = yeoman.Base.extend({
-   
-    prompting: {
-
-        pageName: function() {
-            var done = this.async();
-
-            var prompt = [{
-                type: 'input',
-                name: 'pageName',
-                message: 'Enter a pageName including the full path.',
-                'default': 'index.html'
-            }];
-
-            this.prompt(prompt, function(response) {
-                this.options.pageName = response.pageName;
-                done();
-            }.bind(this));
-        },
-
-        navigation: function() {
-            var done = this.async();
-
-            var prompt = [{
-                type: 'list',
-                name: 'navigation',
-                message: 'Select the navigation you want',
-                'default': 'Slide in off-canvas left',
-                choices: ['Slide in off-canvas left', 'Slide in off-canvas right', 'Left opoup menu', 'Right popup menu']
-            }];
-
-            this.prompt(prompt, function(response) {
-                this.options.navigation = response.navigation;
-                done();
-            }.bind(this));
-        },
-
-        pageTransition: function() {
-            var done = this.async();
-
-            var prompt = [{
-                type: 'list',
-                name: 'pageTransition',
-                message: 'Select the page transition you want',
-                'default': 'Page wise scrolling',
-                choices: ['Page wise scrolling', 'Infinte scrolling']
-            }, {
-                when: function(response) {
-                    if (response.pageTransition === 'Page wise scrolling') {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-                type: 'input',
-                name: 'numberOfPages',
-                message: 'How many pages do you want to include?',
-                'default': 5
-            }];
-            this.prompt(prompt, function(response) {
-                this.options.pageTransition = response.pageTransition;
-                this.options.numberOfPages = response.numberOfPages;
-                done();
-            }.bind(this));
-        },
-
-        components: function() {
-            var done = this.async();
-
-            var prompt = [{
-                type: 'checkbox',
-                name: 'components',
-                message: 'Select components for the page',
-                'default': ['Carousel'],
-                choices: ['Carousel', 'Card', 'Video', 'Parallax']
-            }];
-
-            this.prompt(prompt, function(response) {
-                this.options.components = response.components;
-                done();
-            }.bind(this));
-        }
-    },
-    writing: {
-        
-    }
-});
-*/
+PageGenerator.prototype.writing = {
+	
+		basicSetup: function() {
+			
+			//1. Create the complete path to the pageName			
+			var absoluteFileName =  process.cwd() + path.sep +  this.options.pageName;			
+			var dir = path.dirname(absoluteFileName);
+			mkdirp(dir);
+			
+			//2. render file
+			var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('G:/_node_workspaces/generator-responsive/page/templates'));
+			var res = env.render('page-transition-vertical.tmpl.nun', this.options);
+			fs.writeFileSync(absoluteFileName, res);
+		}
+	
+	
+};
